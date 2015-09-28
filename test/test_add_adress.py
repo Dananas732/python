1a1837e0c58d81a966d0280170bf4bd60f45ad8c
Dananas732 <egorlizlov@gmail.com>
2015-09-28 16:58:18 +0300

    Изменение метода создания адреса для повышения читаемости

--- test/test_add_adress.py
+++ test/test_add_adress.py
@@ -11,5 +11,5 @@ def app(request):
 
 def test_add_address(app):
         app.session.login(username="admin", password="secret")
-        app.add_new_address(add_address(firstname="111", middlename="222", lastname="333", nickname="444", title="555", company="666", address="777"))
+        app.address.create(add_address(firstname="111", middlename="222", lastname="333", nickname="444", title="555", company="666", address="777"))
         app.session.logout()
