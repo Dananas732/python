cdc7996934468850025351bcf4f267e852a4a395
Dananas732 <egorlizlov@gmail.com>
2015-09-28 16:24:52 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_adress.py
+++ test/test_add_adress.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from address import add_address
-from application import Application
+from model.address import add_address
+from pixture.application import Application
 
 @pytest.fixture
 def app(request):
