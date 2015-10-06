2afc05b1b9eaf92519683e65d0f07b4b00969121
Dananas732 <egorlizlov@gmail.com>
2015-10-06 13:50:01 +0300

    Улучшение метода изменения группы

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,7 +1,14 @@
 __author__ = 'e.lyzlov'
 from model.group import Group
 
-def test_modify_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="popopo", header="pipipi", footer="papapa"))
+def test_modify_group_name(app):
+    app.group.modify(Group(name="popopo"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.group.modify(Group(header="pipipi"))
+    app.session.logout()
+
+def test_modify_group_footer(app):
+    app.group.modify(Group(footer="papapa"))
     app.session.logout()
