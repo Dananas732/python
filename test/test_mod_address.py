9dcc9a486996a1b9965faaebc5a4c1cd2a2fdae2
Dananas732 <egorlizlov@gmail.com>
2015-10-08 16:42:26 +0300

    Добавлена проверка на наличие адреса и группы при редактировании и удалении

--- test/test_mod_address.py
+++ test/test_mod_address.py
@@ -2,9 +2,14 @@ __author__ = 'e.lyzlov'
 
 from model.address import add_address
 
+def check_for_address(app):
+    if app.address.count() == 0:
+        app.address.create(add_address(firstname="test"))
+
 def test_modify_address_name(app):
+    check_for_address(app)
     app.address.modify(add_address(firstname="100", middlename="200", lastname="300", nickname="400"))
 
-
 def test_modify_address_company(app):
+    check_for_address(app)
     app.address.modify(add_address(company="900"))
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,14 +1,19 @@
 __author__ = 'e.lyzlov'
 from model.group import Group
 
+def check_for_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
 
 def test_modify_group_name(app):
+    check_for_group(app)
     app.group.modify(Group(name="popopo"))
 
-
 def test_modify_group_header(app):
+    check_for_group(app)
     app.group.modify(Group(header="pipipi"))
 
 
 def test_modify_group_footer(app):
+    check_for_group(app)
     app.group.modify(Group(footer="papapa"))
