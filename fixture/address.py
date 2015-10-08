94f660d2899f654f2f6556ef6c64132f684f6a68
Dananas732 <egorlizlov@gmail.com>
2015-10-08 13:06:46 +0300

    Как-то так сейчас все выглядит, что не открывается стартовая страница даже

--- fixture/address.py
+++ fixture/address.py
@@ -16,30 +16,22 @@ class AddressHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
-    def fill_address_form(self, add_address):
+    def fill_address_form(self, address):
         wd = self.app.wd
-        # fill address form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(add_address.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(add_address.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(add_address.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(add_address.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(add_address.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(add_address.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(add_address.address)
+        self.change_field_value("firstname", address.name)
+        self.change_field_value("middlename", address.header)
+        self.change_field_value("lastname", address.footer)
+        self.change_field_value("nickname", address.name)
+        self.change_field_value("title", address.header)
+        self.change_field_value("company", address.footer)
+        self.change_field_value("address", address.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def return_to_home_page(self):
         wd = self.app.wd
--- test/test_mod_address.py
+++ test/test_mod_address.py
@@ -2,5 +2,9 @@ __author__ = 'e.lyzlov'
 
 from model.address import add_address
 
-def test_modify_address (app):
-        app.address.modify(add_address(firstname="aaa", middlename="bbb", lastname="ccc", nickname="ddd", title="eee", company="fff", address="ggg"))
+def test_modify_address_name(app):
+    app.address.modify(add_address(firstname="aaa", middlename="bbb", lastname="ccc", nickname="ddd"))
+
+
+def test_modify_address_company(app):
+    app.address.modify(add_address(company="fff"))
