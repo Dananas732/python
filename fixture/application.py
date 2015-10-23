638ad0ffbddf15d4b2392988342bd613ffb86eb1
Dananas732 <egorlizlov@gmail.com>
2015-10-23 17:45:04 +0300

    Реализованы опции командной строки, позволяющие указывать браузера и УРЛ

--- fixture/application.py
+++ fixture/application.py
@@ -1,18 +1,26 @@
 __author__ = 'e.lyzlov'
 
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.address import AddressHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "IE":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognize browser %s" %browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.address = AddressHelper(self)
-        self.open_home_page()
+#        self.open_home_page()
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +32,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_css_selector("form[name='MainForm'] input[value='Delete']"))> 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
